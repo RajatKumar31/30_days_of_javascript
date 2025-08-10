070ca5c33fd06d0120eb1e5d1851aceb8d2c41be
rajat kumar <rajat kumar>
2025-08-10 21:13:22 +0530

    solve : implement reduce function

---
+++ Day-6.js
@@ -0,0 +1,15 @@
+//2626. Array Reduce Transformation
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+	let ans = init;
+	for (let i = 0; i < nums.length; i++) {
+		ans = fn(ans, arr[i]);
+	}
+	return ans;
+};
